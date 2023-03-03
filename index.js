d4036aa056759761fdce5bcc6e6f969d62f6aff5
Ankit <ankitpal28090016@gmail.com>
2023-03-04 01:38:41 +0530

    final commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const input = document.getElementById("numOfWords");
+const container = document.querySelector(".container");
+
+const generateWord = (n) => {
+    let text = "";
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for(let i=0; i<n ; ++i){
+
+        const random = (Math.random() * 25).toFixed(0);
+        text += letters[random];
+        // console.log(random);
+    }
+
+    return text;
+};
+
+
+
+let numOfwords ;
+const generatePara = () => {
+    numOfwords = Number(input.value);
+
+
+    const para = document.createElement("p");
+     
+    let data = "";
+    
+    for(let i = 0; i < numOfwords; ++i){
+        const randomNumber = (Math.random() * 15).toFixed(0);
+        data += generateWord(randomNumber);
+        data += " ";
+    }
+
+    para.innerText = data;
+
+    para.setAttribute("class", "paras");
+
+    container.append(para);
+};
+
+
+
